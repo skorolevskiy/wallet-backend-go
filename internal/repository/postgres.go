c08f2d02e1e570dd87a4c7a982c360c6cdac2fa0
skorolevsky <=]Zh@\rXS2KCqHG>
2022-12-09 00:01:54 +0200

    make db connection

---
+++ internal/repository/postgres.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	usersTable       = "users"
+	walletsTable     = "wallets"
+	transactionTable = "transactions"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode))
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
